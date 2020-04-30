968058e8f8aa234178128b2e6a6ed6e08dc8bf00
GebelGerman <gebel.german@yahoo.com>
2020-04-30 15:56:24 +0500

    parser_v4

--- parsers/parser.py
+++ parsers/parser.py
@@ -2,12 +2,10 @@ from bs4 import BeautifulSoup
 import requests as req
 import csv
 
-global_data = []
-
 
 class Parser:
     @staticmethod
-    def get_soup(url, params=None):
+    def get_soup(url, params=None) -> BeautifulSoup:
         try:
             page = req.post(url, data=params)
             # with open('page.html', 'w') as file:  # это для себя
@@ -20,22 +18,36 @@ class Parser:
 
 
 class Zachestnyibiznes(Parser):
-    # def __init__(self):
-    #     self.data = []
 
     @staticmethod
-    def get_params_for_finance(soup):
-        global global_data
-        global_data.append(["Наименование компании", soup.find(id='nameCompCard').text.split('\n')[0]])
-        global_data.append(['Оценка', soup.find(class_="box-rating box-rating-success").text])
-        global_data.append(['Статус', soup.find('b', class_="").text])
-        global_data.append(['Дата регистрации', soup.find(itemprop="foundingDate").text])
-        cap = [i.text.split("Уставный капитал:") for i in soup.find_all('small') if "Уставный капитал:" in i.text]
-        global_data.append(['Уставный капитал', cap[0]])
+    def get_other_data(soup_company: BeautifulSoup):  # находим параметры другие параметры, такие как статус и тд
+        other_data = [["Наименование компании", soup_company.find(id='nameCompCard').text.split('\n')[0]]]
+        try:  # Оценки может не быть
+            other_data.append(['Оценка', soup_company.find(class_="box-rating").text])
+        except:
+            other_data.append(['Оценка', '-'])
 
-        okpo = soup.find('span', id="okpo").text
-        inn = soup.find('span', id="inn").text
-        date = soup.find(id='ogrn').find_next('a').text.split('.')[2]
+        founding_date = soup_company.find(itemprop="foundingDate")
+        other_data.append(['Дата регистрации', founding_date.text.split('ЗАЧЕСТНЫЙБИЗНЕС\n')[1]])
+        status = founding_date.find_previous('b').text
+        other_data.append(['Статус', status])
+        try:
+            capital = \
+            soup_company.find(itemprop='founder').find_previous('small').text.split('ЗАЧЕСТНЫЙБИЗНЕС')[1].split(' руб')[
+                0]
+            other_data.append(['Уставный капитал', capital])
+        except:
+            other_data.append(['Уставный капитал', 0])
+        return other_data
+
+    @staticmethod
+    def get_params_for_finance(soup_company):  # находим параметры для юрл финансовой таблицы
+        try:  # окпо есть не у всех компаний
+            okpo = soup_company.find('span', id="okpo").text
+        except:
+            okpo = ''
+        inn = soup_company.find('span', id="inn").text
+        date = soup_company.find(id='ogrn').find_next('a').text.split('.')[2]
         return {
             'okpo': okpo,
             'inn': inn,
@@ -43,22 +55,33 @@ class Zachestnyibiznes(Parser):
         }
 
     @staticmethod
-    def parse_num(s: str):
+    def parse_num(s: str):  # парсер чисел в финансовой таблице
         return ''.join(s[1:].split(' '))
 
     @staticmethod
-    def url_finance_maker(params: dict):
+    def make_url_finance_table(params: dict):  # создаем юрл для финансовой таблицы
         return 'https://zachestnyibiznes.ru/company/balance?okpo={}&inn={}&date={}&page='.format(params['okpo'],
                                                                                                  params['inn'],
                                                                                                  params['date'])
 
     @staticmethod
-    def collect_company_data(soup: BeautifulSoup):
-        global global_data
+    def find_company(inn: str):  # находим компанию в поисковой строке по ИНН и возвращаем soup страницы компании
+        url = 'https://zachestnyibiznes.ru/search?query={}'.format(inn)
+        soup = Zachestnyibiznes.get_soup(url)
+        status = soup.find('a', itemprop='legalName').find_next('td').find_next('span').text[1:]
+        if status == 'Ликвидировано ':
+            return None
+        return Parser.get_soup('https://zachestnyibiznes.ru' + soup.find(itemprop='legalName')['href'])
+
+    @staticmethod
+    def collect_company_data(soup_finance_table: BeautifulSoup,
+                             other_data):  # сборка всех данных о компании в один список
+        # данные с основной страницы компании
         data = [[]]
-        for i in global_data:
+        for i in other_data:
             data.append(i)
-        table = soup.find('div', id='fin-stat').find('table')
+        # данные из таблицы
+        table = soup_finance_table.find('div', id='fin-stat').find('table')
         thead = table.find('thead')
         data.append([i.text for i in thead.find_all('th')])
         tbody = table.find('tbody')
@@ -72,29 +95,33 @@ class Zachestnyibiznes(Parser):
                     s = Zachestnyibiznes.parse_num(s)
                 mini_data.append(s)
             data.append(mini_data)
+        data.append([])
         return data
 
 
 class Rusprofile(Parser):
     @staticmethod
-    def get_inn(soup: BeautifulSoup):
-        return soup.find(id='clip_inn').text
+    def get_inn(soup_company: BeautifulSoup):  # берем ИНН
+        try:
+            title = soup_company.find('title').text
+            return title.split('ИНН ')[1].split(',')[0]
+        except:
+            return None
 
     @staticmethod
-    def get_url_company(id):
+    def make_url_company(id):
         return 'https://www.rusprofile.ru/id/{}'.format(id)
 
     # @staticmethod
     # def run_urls():
     #     for i in range(max(int)):
     #         try:
-    #             return req.get(Rusprofile.get_url_company(i))
+    #             return req.get(Rusprofile.make_url_company(i))
     #         except TimeoutError:
     #             return None
 
 
-
-class Audit(Parser):
+class Audit(Parser):  # больше данных чем на Зачестный бизнес
     pass
 
 
@@ -105,11 +132,26 @@ def csv_writer(data, path):
             writer.writerow(line)
 
 
-if __name__ == '__main__':
+def parse_data(id):  # пошагойвый парсинг данных одной компании
+    url = Rusprofile.make_url_company(id)
+    soup = Parser.get_soup(url)
+    inn = Rusprofile.get_inn(soup)
+    if not inn is None:
+        soup_company = Zachestnyibiznes.find_company(inn)
+        if not soup_company is None:
+            Zachestnyibiznes.get_other_data(soup_company)
+            params = Zachestnyibiznes.get_params_for_finance(soup_company)
+            other_data = Zachestnyibiznes.get_other_data(soup_company)
+            fin_soup = Parser.get_soup(Zachestnyibiznes.make_url_finance_table(params))
+            data = Zachestnyibiznes.collect_company_data(fin_soup, other_data)
+            csv_writer(data, '../../data.csv')
 
-    url_company = 'https://zachestnyibiznes.ru/company/ul/1027700092661_7712040126_PAO-AEROFLOT'  # пример
-    params = Zachestnyibiznes.get_params_for_finance(Parser.get_soup(url_company))
-    fin_soup = Parser.get_soup(Zachestnyibiznes.url_finance_maker(params))
-    data = Zachestnyibiznes.collect_company_data(fin_soup)
-    csv_writer(data, '../../data.csv')
+
+if __name__ == '__main__':
+    # for i in range(5):
+    #     url = Rusprofile.make_url_company(i)
+    #     soup = Parser.get_soup(url)
+    #     inn = Rusprofile.get_inn(soup)
+    #     print(inn)
+    parse_data(213123)
     print('FINISH!!!!!!!!!!')
